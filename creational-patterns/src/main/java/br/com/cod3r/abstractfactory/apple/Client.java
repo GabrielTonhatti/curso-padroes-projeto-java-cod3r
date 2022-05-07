fbb674e0ce4010f7647f15fb1c6dd64d89033877
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-05-07 13:59:27 -0300

    Abstract Factory

--- creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/abstractfactory/apple/Client.java
@@ -1,8 +1,28 @@
 package br.com.cod3r.abstractfactory.apple;
 
+import br.com.cod3r.abstractfactory.apple.factory.IPhone11Factory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneFactory;
+import br.com.cod3r.abstractfactory.apple.factory.IPhoneXFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.BrazilianRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.CountryRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.factory.abstractFactory.USRulesAbstractFactory;
+import br.com.cod3r.abstractfactory.apple.model.iphone.IPhone;
+
 public class Client {
 	
 	public static void main(String[] args) {
+		CountryRulesAbstractFactory rules = new BrazilianRulesAbstractFactory();
+//		CountryRulesAbstractFactory rules = new USRulesAbstractFactory();
+		IPhoneFactory genXFactory = new IPhoneXFactory(rules);
+		IPhoneFactory gen11Factory = new IPhone11Factory(rules);
+
+		System.out.println("### Ordering an iPhone X");
+		IPhone iPhone = genXFactory.orderIPhone("standard");
+		System.out.println(iPhone);
+
+		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+		IPhone iPhone2 = gen11Factory.orderIPhone("highEnd");
+		System.out.println(iPhone2);
 
 	}
 }
--- creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/after/Client.java
+++ creational-patterns/src/main/java/br/com/cod3r/factory/app/dbadapter/after/Client.java
@@ -5,10 +5,11 @@ import br.com.cod3r.factory.app.dbadapter.after.factory.OracleFactory;
 
 public class Client {
 
-	public static void main(String[] args) {
-//		DB db = new PostgresFactory().getDatabase();
-		DB db = new OracleFactory().getDatabase();
-		db.query("SELECT * FROM users");
-		db.update("INSERT INTO users VALUES ('User', 25)");
-	}
+    public static void main(String[] args) {
+//		DB postgres = new PostgresFactory().getDatabase();
+        DB db = new OracleFactory().getDatabase();
+        db.query("SELECT * FROM users");
+        db.update("INSERT INTO users VALUES ('User', 25)");
+    }
+
 }
